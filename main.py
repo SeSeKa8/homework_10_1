74706eb358d5daf6dc98fa0686c253fdf46b6e61
Sergei Kalinichenko <ssk98@mail.ru>
2024-11-08 22:35:48 +0700

    add dir and module from home work poetry

---
+++ main.py
@@ -0,0 +1,11 @@
+# Теперь импортируем функции из masks.py
+from masks import get_mask_account, get_mask_card_number
+
+# Используем функции
+if __name__ == "__main__":
+    # Пример вызова функций
+    card_number = 7000792289606361
+    account_number = 73654108430135874305
+
+    print(get_mask_card_number(str(card_number)))
+    print(get_mask_account(str(account_number)))
